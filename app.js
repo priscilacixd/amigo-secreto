e16a4ae8cc2a08e0c41341c1b6bc619e79033246
Priscila Carla <125103409+priscilacixd@users.noreply.github.com>
2025-01-31 00:33:49 -0300

    finalizando functions

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function getListaAmigos() {
+    return document.getElementById("listaAmigos");
+}
+
+function adicionarAmigo() {
+    let amigoInserido = document.getElementById("amigo").value; 
+    console.log(amigoInserido);
+
+    if (amigoInserido == "") {
+        alert("Por favor insira um nome");
+    } else {
+        amigos.push(amigoInserido);
+        console.log(amigos);
+        exibirAmigos();
+    }
+    limparCampoInputAmigo(); 
+}
+
+function exibirAmigos() {
+    listaAmigos = getListaAmigos();
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`; 
+    }
+}
+
+function limparCampoInputAmigo() {
+    let inputAmigo = document.getElementById("amigo"); 
+    inputAmigo.value = "";
+}
+
+function gerarIndiceAleatorio() {
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    return indiceAleatorio;
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        alert("Lista de amigos vazia, adicione amigos.");
+    }
+    else {
+        listaAmigos = getListaAmigos();
+        listaAmigos.innerHTML = "";
+        let resultadoAmigo = document.getElementById("resultado");
+        resultadoAmigo.innerHTML = `<li>${amigos[gerarIndiceAleatorio()]}</li>`;
+    }
+}
+
+//limpar lista ul que exibe nome dos amigos assim que sortear
+//limpar resultadoAmigo após sortear e for adicionar outro nome - mudar o botao para novo amigo secreto ou criar um novo botao que ativa quando sortear pela primeira vez.
+
+
